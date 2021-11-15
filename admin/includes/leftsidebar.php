1e98d71466c06e9e195cda98c9d433fa390ffeab
Le Duc Anh <ducanh1818181@gmail.com>
2021-11-15 19:42:04 +0700

    up file

---
+++ admin/includes/leftsidebar.php
@@ -0,0 +1,73 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">Navigation</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Bảng điều khiển </span> </a>
+                         
+                            </li>
+
+
+
+               
+
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Sự kiện </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                	<li><a href="add-category.php">Thêm sự kiện</a></li>
+                                    <li><a href="manage-categories.php">Quản lý sự kiện</a></li>
+                                </ul>
+                            </li>
+
+    <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>Sự kiện phụ </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-subcategory.php">Thêm sự kiện phụ</a></li>
+                                    <li><a href="manage-subcategories.php">Quản lý sự kiện phụ</a></li>
+                                </ul>
+                            </li>               
+  <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Bài viết </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-post.php">Thêm bài viết</a></li>
+                                    <li><a href="manage-posts.php">Quản lý bài viết</a></li>
+                                     <li><a href="trash-posts.php">Bài viết đã xóa</a></li>
+                                </ul>
+                            </li>  
+                     
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Trang </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="aboutus.php">Về chúng tôi</a></li>
+                                    <li><a href="contactus.php">Liên hệ</a></li>
+                                </ul>
+                            </li>
+   <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Bình luận </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.php">Chờ xét duyệt </a></li>
+                                    <li><a href="manage-comments.php">Đã xét duyệt</a></li>
+                                </ul>
+                            </li>   
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+                    <div class="help-box">
+                        <h5 class="text-muted m-t-0">For Help ?</h5>
+                        <p class=""><span class="text-custom">Email:</span> <br/> haibanchai@gmail.com</p>
+                        <p class=""><span class="text-custom">Tel:</span> <br> 0369668700</p>
+                    </div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
